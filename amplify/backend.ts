39938e4e5ae4dbdb9164e42b5493297eab637c89
Avinash Verma <avinash000verma@gmail.com>
2024-08-07 12:02:50 +0530

    for backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,26 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-// import {hippaContractData} from './data/hippaContract.js';
-// import { aws_dynamodb } from "aws-cdk-lib";
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
 });
-
-
-// const externalDataSourcesStack = backend.createStack("MyExternalDataSources");
-
-
-// const externalTable = aws_dynamodb.Table.fromTableName(
-//   externalDataSourcesStack,
-//   "MyHippaContractTable",
-//   "hippaContractTable"
-// );
-
-
-// backend.data.addDynamoDbDataSource(
-//   "hippaContractTable",
-//   externalTable
-// );
