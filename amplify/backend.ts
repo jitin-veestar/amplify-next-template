3483c82583dd792cd775a4fea19d8b0c707093c6
Jitin Rathi <jitinrathi910949@gmail.com>
2024-09-15 02:17:16 +0530

    backend function check

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { sendLinkEmail } from './functions/send-link-email/resource.js';
+import { sendLinkEmail } from './functions/send-link-email/resource';
 defineBackend({
   auth,
   data,
